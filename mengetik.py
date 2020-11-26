598096e192e8b34608368f90c723288a3fae0330
Teyak <74811879+Teyak@users.noreply.github.com>
2020-11-26 10:39:48 +0700

    Create mengetik.py

---
+++ mengetik.py
@@ -0,0 +1,18 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('terima kasih telah memakai tools ini...')
+mengetik('pakailah tools ini dengan baik dan bijak:)')
+mengetik('dan jangan disalah gunakan:/')
+mengetik('menjadi hacker tidak harus merusak')
+mengetik('tapi kita bisa menjadi hacker yang propesional')
+mengetik('salam Teyak-Team')
+mengetik('bye:)')
+mengetik('...')
+mengetik('....')
+mengetik('.....')
